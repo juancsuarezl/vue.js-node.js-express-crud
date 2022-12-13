f7a72e82e5f7f1eecbeb12b7390fcac23fa1d0c9
Juan Carlos Suárez <juancsuarezl@gmail.com>
2022-12-13 10:26:51 -0300

    Cambios en la aplicación

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '',
-    database : 'testdb'
+    database : 'test'
   });
 
   //Establecemos la conexión 
